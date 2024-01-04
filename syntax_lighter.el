92cb56ee05a158edd3a1b363ef9549808d922e02
nayoungwook <nayoungwoook@gmail.com>
2024-01-04 15:18:17 +0900

    initial commit

---
+++ syntax_lighter.el
@@ -0,0 +1,24 @@
+;; chsetnut-highlights mode
+
+(setq chestnut-font-lock-keywords
+	  (let* (
+			 (x-keywords '("if" "else" "for" "while" "break" "continue" "return"
+						   "this" "func" "var" "new" "class" "import"
+						   "private" "public" "protected"))
+			 (x-types '("number" "string" "character" "bool" "void"))
+			 
+             (x-keywords-regexp (regexp-opt x-keywords 'words))
+             (x-types-regexp (regexp-opt x-types 'words))
+             )
+        `(
+          (,x-types-regexp . 'font-lock-type-face)
+          (,x-keywords-regexp . 'font-lock-keyword-face)
+		  )
+		)
+	  )
+
+(define-derived-mode chestnut-mode c-mode "chestnut mode"
+
+  (setq font-lock-defaults '((chestnut-font-lock-keywords))))
+
+(provide 'chestnut-mode)
