c86c6cfe9b3edceb9528ef7fefdab18d49eca385
nayoungwook <nayoungwoook@gmail.com>
2024-02-05 07:37:17 +0900

    .

--- syntax_lighter.el
+++ syntax_lighter.el
@@ -1,11 +1,15 @@
-;; chsetnut-highlights mode
+;; Chsetnut-highlights mode
 
 (setq chestnut-font-lock-keywords
 	  (let* (
-			 (x-keywords '("if" "else" "for" "while" "break" "continue" "return"
+			 (x-keywords '("if" "else" "for" "while" "break"
+						   "continue" "return"
 						   "this" "func" "var" "new" "class" "import"
-						   "private" "public" "protected"))
-			 (x-types '("number" "string" "character" "bool" "void"))
+						   "private" "public" "protected" "true" "false"
+						   "extends" "constructor" "static" "scene" "null" "object"
+						   ))
+			 (x-types '("number" "string" "character" "bool" "void"
+						"float" "int" "double"))
 			 
              (x-keywords-regexp (regexp-opt x-keywords 'words))
              (x-types-regexp (regexp-opt x-types 'words))
